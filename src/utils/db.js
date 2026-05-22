2853b98e3464f4de9ee9b249f66c328d0e3c8805
daipeng <2808707765@qq.com>
2026-05-22 10:43:33 +0800

    chore: update dbConfig to use online address universally

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,8 @@
 const mysql = require('mysql2/promise')
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const dbConfig = {
-	host: isProd ? '10.34.112.115' : 'sh-cynosdbmysql-grp-h10adil8.sql.tencentcdb.com',
-	port: isProd ? 3306 : 29922,
+	host: 'sh-cynosdbmysql-grp-h10adil8.sql.tencentcdb.com',
+	port: 29922,
 	user: 'root',
 	password: 'QhLcnFGrpFjfzL5'
 }
